08ba8ecd5c06db83d8e9efd9d0a56691b577fd14
[Abdelbarie Raissouni] <rabdlbari@gmail.com>
2025-11-24 20:19:06 +0100

    created the landing page and its component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function RootLayout({
   children,
@@ -9,7 +18,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body
+          className={`${poppins.variable} flex flex-col h-screen`}
+        >
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,55 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { BookUser, Building2 } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
-  return <main className="flex h-screen justify-center items-center "></main>;
+  return (
+    <>
+      <header className="flex justify-center w-6xl mx-auto py-2 px-6 sticky top-0 z-50 border rounded-full mt-2 bg-white/80 backdrop-blur-xl dark:bg-black/80">
+        <div className="flex gap-4">
+          <Button>
+            <Link href={"/login"} className="hover:underline">
+              Login
+            </Link>
+          </Button>
+          <Button variant={"secondary"}>
+            <Link href={"/signup"} className="hover:underline">
+              Sign up
+            </Link>
+          </Button>
+        </div>
+      </header>
+      <main className="flex flex-col h-full gap-4 justify-center items-center">
+        <div className="text-center w-3xl space-y-4">
+          <h1 className="text-6xl text-center font-extrabold tracking-tight text-balance">
+            A Simple{" "}
+            <span className="text-transparent [-webkit-text-stroke:1px_black]">
+              Dashboard
+            </span>{" "}
+            for Agencies and Contacts
+          </h1>
+          <p className="text-base text-gray-700">
+            Securely access agency data, browse employee contacts, and manage
+            your daily usage with a streamlined, login-protected dashboard.
+          </p>
+          <Separator />
+        </div>
+        <div className="flex gap-4">
+          <Button size={"lg"}>
+            <BookUser />
+            <Link href={"/signup"} className="hover:underline">
+              View Contacts
+            </Link>
+          </Button>
+          <Button size={"lg"}>
+            <Building2 />
+            <Link href={"/signup"} className="hover:underline">
+              View Agencies
+            </Link>
+          </Button>
+        </div>
+      </main>
+    </>
+  );
 }

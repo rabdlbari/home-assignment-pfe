6e487854cfd376eedbdc03cd8d48b7f7c73a1f6c
[Abdelbarie Raissouni] <rabdlbari@gmail.com>
2025-11-25 14:40:34 +0100

    added the files for the routes and sidebar component

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,45 @@
+import { ClerkProvider, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import React from "react";
+import { Poppins } from "next/font/google";
+import "../globals.css";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${poppins.variable} flex flex-col h-screen`}>
+          <header className="flex justify-between items-center w-full py-2 px-6 border bg-white/80 backdrop-blur-xl dark:bg-black/80">
+            <div className="font-bold text-xl">A.R</div>
+            <SignedOut>
+              <div className="flex gap-4">
+                <Button>
+                  <Link href={"/login"} className="hover:underline">
+                    Login
+                  </Link>
+                </Button>
+                <Button variant={"secondary"}>
+                  <Link href={"/signup"} className="hover:underline">
+                    Sign up
+                  </Link>
+                </Button>
+              </div>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher("/dashboard(.*)");
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/contacts(.*)",
+  "/agencies(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
@@ -8,9 +12,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
